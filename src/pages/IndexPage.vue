15dcd801fc158a74b76c0eea241dfca201e8c1f8
tontanagon <tontanagon555@gmail.com>
2023-10-04 11:52:12 +0700

    finish

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,17 +2,25 @@
   <div class="htmlClass">
     <h1>{{ title }}</h1>
   </div>
+  <InputPage></InputPage>
+  <FunctionPage></FunctionPage>
+
+
 </template>
 
 
 <script>
+import InputPage from "./InputPage.vue";
+import FunctionPage from "./FunctionPage.vue";
+
 export default {
-  name: 'HtmlComponent',
-  data() {
-    return {
-      title: 'Vue is awesome.',
-    }
-  },
+    name: 'HtmlComponent',
+    data() {
+        return {
+            title: 'ธนกร แย้มศิริ',
+        };
+    },
+    components: { InputPage,FunctionPage }
 }
 </script>
 
